6f4236cc26ae15f61d55c72f402554ea580730e4
Amit Ganuwala <135004494+sonalhingnekar100@users.noreply.github.com>
2024-02-15 12:04:42 +0530

    sonal
    Test

---
+++ web/frontend/pages/index.jsx
@@ -0,0 +1,30 @@
+import {
+  Card,
+  Page,
+  Layout,
+  TextContainer,
+  Image,
+  Stack,
+  Link,
+  Text,
+} from "@shopify/polaris";
+import { TitleBar } from "@shopify/app-bridge-react";
+import { useTranslation, Trans } from "react-i18next";
+
+import { trophyImage } from "../assets";
+
+import { ProductsCard } from "../components";
+
+export default function HomePage() {
+  const { t } = useTranslation();
+  return (
+    <Page narrowWidth>
+      <TitleBar title={t("Testing app")} primaryAction={null} />
+      <Layout>
+        <Layout.Section>
+          <ProductsCard />
+        </Layout.Section>
+      </Layout>
+    </Page>
+  );
+}
